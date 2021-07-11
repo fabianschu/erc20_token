90a7aee385b6d72b88eeb1ae48bca8bf68d93d30
fabianschu <48454910+fabianschu@users.noreply.github.com>
2021-07-11 22:26:51 +0200

    adds minting functionality

--- tasks/mint.ts
+++ tasks/mint.ts
@@ -10,10 +10,11 @@ task("mintTokens", "Mints tokens")
       { amount, beneficiary: address }: any,
       { ethers }: HardhatRuntimeEnvironment
     ) => {
+      console.log(
+        `Mint ${amount} FuschuTokens to following address: ${address}`
+      );
       const fuschuTokenInstance = await ethers.getContract("FuschuToken");
-      console.log(amount);
-      console.log(typeof amount);
-      const mintAmount = utils.parseEther(amount);
+      const mintAmount = utils.parseEther(amount.toString());
       const tx = await fuschuTokenInstance.mint(address, mintAmount);
       console.log("Transaction:", tx.hash);
     }
